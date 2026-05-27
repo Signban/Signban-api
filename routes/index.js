54bac1c69c80d8cda1bda1d9428fbc71eb82e0e6
ramaatha <ramabm18@gmail>
2026-05-28 00:06:49 +0700

    feat: add boards and board members endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const authRoute = require("./auth");
+const boardsRoute = require("./boards");
 
 router.use(authRoute);
+router.use(boardsRoute);
 
 router.get("/", (req, res) => {
   res.status(200).json("home");
