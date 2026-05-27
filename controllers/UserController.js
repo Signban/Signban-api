543ebd314366a14cb022861cc317dc0c3029bbe1
lluthfiamalia <lluthfiamalia14@gmail.com>
2026-05-27 21:55:07 +0700

    feat: add register endpoint and password hashing hook

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,41 +5,58 @@ const { errorName } = require("../helpers/enums");
 const { AppError } = require("../models/utils/class");
 
 class UserController {
-	static async login(req, res, next) {
-		try {
-			const { email, password } = req.body;
+  static async register(req, res, next) {
+    try {
+      const { name, email, password } = req.body;
 
-			if (!email) throw new AppError(errorName.BadRequest, "email is required");
-			if (!password)
-				throw new AppError(errorName.BadRequest, "password is required");
+      const user = await User.create({ name, email, password });
 
-			const user = await User.findOne({ where: { email } });
-			if (!user)
-				throw new AppError(errorName.Unauthorized, "invalid email/password");
+      res.status(201).json({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        avatarUrl: user.avatarUrl,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 
-			const isValid = checkPassword(password, user.password);
-			if (!isValid)
-				throw new AppError(errorName.Unauthorized, "invalid email/password");
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
 
-			const access_token = signToken({
-				id: user.id,
-				email: user.email,
-			});
+      if (!email) throw new AppError(errorName.BadRequest, "email is required");
+      if (!password)
+        throw new AppError(errorName.BadRequest, "password is required");
 
-			res.status(200).json({
-				access_token,
-			});
-		} catch (error) {
-			next(error);
-		}
-	}
-	static async getCurrentUser(req, res, next) {
-		try {
-			res.status(200).json(req.user);
-		} catch (error) {
-			next(error);
-		}
-	}
+      const user = await User.findOne({ where: { email } });
+      if (!user)
+        throw new AppError(errorName.Unauthorized, "invalid email/password");
+
+      const isValid = checkPassword(password, user.password);
+      if (!isValid)
+        throw new AppError(errorName.Unauthorized, "invalid email/password");
+
+      const access_token = signToken({
+        id: user.id,
+        email: user.email,
+      });
+
+      res.status(200).json({
+        access_token,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async getCurrentUser(req, res, next) {
+    try {
+      res.status(200).json(req.user);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = UserController;
