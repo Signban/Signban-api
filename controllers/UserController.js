5d15d4e30487815ee6162f5e240e18093c56658e
brahmantiodiaz <auliabrahmantio@gmail.com>
2026-05-27 15:45:29 +0700

    feat: add checkCurrenUser routes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -33,6 +33,13 @@ class UserController {
 			next(error);
 		}
 	}
+	static async getCurrentUser(req, res, next) {
+		try {
+			res.status(200).json(req.user);
+		} catch (error) {
+			next(error);
+		}
+	}
 }
 
 module.exports = UserController;
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const UserController = require("../controllers/UserController");
+const authentication = require("../middlewares/authentication");
 
 const router = require("express").Router();
 
 router.post("/login", UserController.login);
+router.get("/current-user", authentication, UserController.getCurrentUser);
 
 module.exports = router;
