00d9150fb302686c34befe2d281a85886c3adb2f
ramaatha <ramabm18@gmail>
2026-05-28 12:17:28 +0700

    feat: add list controller and endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const authRoute = require("./auth");
 const boardsRoute = require("./boards");
+const listsRoute = require("./lists");
 
 router.use(authRoute);
 router.use(boardsRoute);
+router.use(listsRoute);
 
 router.get("/", (req, res) => {
   res.status(200).json("home");
 });
-
+9;
 module.exports = router;
