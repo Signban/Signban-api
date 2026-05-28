6929761a03ac89a1d433aeb10be8d66c771a9317
Aulia Brahmantio Diaz <55686668+brahmantiodiaz@users.noreply.github.com>
2026-05-29 03:05:48 +0700

    feat: add card cover upload route

--- routes/card.js
+++ routes/card.js
@@ -1,41 +1,47 @@
 const router = require("express").Router();
 const authentication = require("../middlewares/authentication");
+const uploadImage = require("../middlewares/uploadImage");
 const CardController = require("../controllers/CardController");
 
 router.use(authentication);
 
 router.get("/boards/:boardId/cards/:cardId", CardController.allCard);
 router.put("/boards/:boardId/cards/:cardId", CardController.updateCard);
+router.patch(
+	"/boards/:boardId/cards/:cardId/cover",
+	uploadImage.single("coverUrl"),
+	CardController.updateCardCover,
+);
 router.post("/boards/:boardId/lists/:listId/cards", CardController.createCard);
 router.patch("/boards/:boardId/cards/:cardId/move", CardController.moveCard);
 router.delete("/boards/:boardId/cards/:cardId", CardController.delCard);
 router.post(
-  "/boards/:boardId/cards/:cardId/comments",
-  CardController.createComment,
+	"/boards/:boardId/cards/:cardId/comments",
+	CardController.createComment,
 );
 router.post(
-  "/boards/:boardId/cards/:cardId/checklists/ai-generate",
-  CardController.generateWithAI,
+	"/boards/:boardId/cards/:cardId/checklists/ai-generate",
+	CardController.generateWithAI,
 );
 router.post(
-  "/boards/:boardId/cards/:cardId/checklists",
-  CardController.createChecklist,
+	"/boards/:boardId/cards/:cardId/checklists",
+	CardController.createChecklist,
 );
 router.patch(
-  "/boards/:boardId/cards/:cardId/checklists/:checklistId",
-  CardController.updateChecklist,
+	"/boards/:boardId/cards/:cardId/checklists/:checklistId",
+	CardController.updateChecklist,
 );
 router.post(
-  "/boards/:boardId/cards/:cardId/assignees",
-  CardController.addAssignee,
+	"/boards/:boardId/cards/:cardId/assignees",
+	CardController.addAssignee,
 );
 router.delete(
-  "/boards/:boardId/cards/:cardId/assignees/:userId",
-  CardController.removeAssignee,
+	"/boards/:boardId/cards/:cardId/assignees/:userId",
+	CardController.removeAssignee,
 );
 router.delete(
-  "/boards/:boardId/cards/:cardId/checklists/:checklistId",
-  CardController.delChecklist,
+	"/boards/:boardId/cards/:cardId/checklists/:checklistId",
+	CardController.delChecklist,
 );
 
 module.exports = router;
