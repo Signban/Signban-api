3e75ce347038d7f6239a3f160b4ef1f088f7749f
lluthfiamalia <lluthfiamalia14@gmail.com>
2026-05-28 10:19:31 +0700

    merge development

--- routes/auth.js
+++ routes/auth.js
@@ -7,20 +7,17 @@ router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/current-user", authentication, UserController.getCurrentUser);
 router.get("/google-login", UserController.googleLogin);
-<<<<<<< HEAD
 router.post("/forgot-password", UserController.forgotPassword);
 router.post(
   "/check-reset-password-token",
   UserController.checkResetPasswordToken,
 );
 router.post("/reset-password", UserController.resetPassword);
-=======
 router.patch("/account/name", authentication, UserController.updateName);
 router.patch(
   "/account/password",
   authentication,
   UserController.updatePassword,
 );
->>>>>>> 6497222a5e97d8415afe28578613fb232f6251c8
 
 module.exports = router;
