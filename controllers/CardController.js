ac4e2e13630604d84166b490b97ec8422ec5a3d0
Aulia Brahmantio Diaz <55686668+brahmantiodiaz@users.noreply.github.com>
2026-05-29 05:44:43 +0700

    fix: avoid mutating sequelize card associations

--- controllers/CardController.js
+++ controllers/CardController.js
@@ -39,14 +39,6 @@ class CardController {
 
 		if (!card) throw new AppError(errorName.NotFound, "Card not found");
 
-		if (card.Checklists) {
-			card.Checklists.sort((a, b) => (a.position || 0) - (b.position || 0));
-		}
-
-		if (card.Comments) {
-			card.Comments.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
-		}
-
 		return card;
 	}
 
